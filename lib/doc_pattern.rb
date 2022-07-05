791220bd9f1b83fb8fda87d5f511956ea4fbe2b7
João <programador09@speedgov.com.br>
2022-07-04 22:41:18 -0300

    creating a unltin line rule, storaging line range

--- lib/doc_pattern.rb
+++ lib/doc_pattern.rb
@@ -1,30 +1,59 @@
 # frozen_string_literal: true
 
-require_relative 'doc_pattern/version'
-require 'json'
-require 'byebug'
+require_relative "doc_pattern/version"
+require "json"
+require "byebug"
 
+# This class represent the validation of a file, based on a json model
 class DocPattern
-  VALID_SUB_KEYS = ['separator', 'field_type', 'field_pattern', 'line_length', 'minimal_elements', 'max_elements'].freeze
+  VALID_SUB_KEYS = %w[separator field_type field_pattern line_length minimal_elements
+                      max_elements].freeze
 
   class Error < StandardError; end
 
   def read_json(path)
     file = File.read(path)
-    @doc_pattern = JSON.parse(file)
+    @doc_pattern ||= JSON.parse(file)
+  end
+
+  def sorted_line_keys
+    @sorted_keys ||= @doc_pattern.keys.select {|key| key.include?('line_')}.sort @doc_pattern.instance_of?(Hash)
+  end
+
+  def until_line?(key)
+    key.include?('..')
+  end
+
+  def until_line_range(key)
+    if until_line?(key)
+      lines = Array.new
+      lines_range = key.tr('line_', '')
+      first_line = lines_range[0].to_i
+      last_line = lines_range[-1].to_i
+
+      for line in first_line..last_line do
+        lines.push(line)
+      end
+
+      return lines
+    end
+    return Array.new
+  end
+
+  def multiple_line(key)
   end
 
   def valid?
-    if @doc_pattern != nil && @doc_pattern.class.name.eql?('Hash')
+    if @doc_pattern.instance_of?(Hash)
       return false if @doc_pattern.keys.empty?
 
-      has_specif_line_rule = @doc_pattern.keys.any? {|key| key.include?('line_') }
-      has_every_line_rule = @doc_pattern.keys.include?('every_line')
+      has_specif_line_rule = @doc_pattern.keys.any? { |key| key.include?("line_") }
+      has_every_line_rule = @doc_pattern.keys.include?("every_line")
 
       return (has_every_line_rule && !has_specif_line_rule) || (!has_every_line_rule && has_specif_line_rule)
     end
 
-    return false
+    false
   end
 
   def invalid?
--- spec/doc_pattern_spec.rb
+++ spec/doc_pattern_spec.rb
@@ -3,14 +3,39 @@
 RSpec.describe DocPattern do
   subject { DocPattern.new }
 
-  it 'valid sub keys should be' do
-    expect(DocPattern::VALID_SUB_KEYS).to eq ["separator", "field_type", "field_pattern", "line_length", "minimal_elements", "max_elements"]
+  it "valid sub keys" do
+    expect(DocPattern::VALID_SUB_KEYS).to eq %w[separator field_type field_pattern line_length
+                                                minimal_elements max_elements]
   end
 
-  describe '#valid?' do 
-    it 'should valid if not had broken rules' do
+  describe "#until_line_range" do
+    context "when there is a range" do
+      valid_key = 'line_1..3'
+
+      it 'should be a until line' do
+        expect(subject.until_line?(valid_key)).to be_truthy
+      end
+      it 'until line should had a range' do
+        expect(subject.until_line_range(valid_key)).to eq [1,2,3]
+      end
+    end
+
+    context "when there is no range" do
+      invalid_key = 'line_1'
+
+      it 'should not be a until line' do
+        expect(subject.until_line?(invalid_key)).to be_falsey
+      end
+      it 'single line should not had a range' do
+        expect(subject.until_line_range(invalid_key)).to eq []
+      end 
+    end
+  end
+
+  describe "#valid?" do
+    it "should valid if not had broken rules" do
       [
-        'lib/json/valids/single_line.json'
+        "lib/json/valids/single_line.json"
       ].each do |directory|
         subject.read_json(directory)
         expect(subject).to be_valid
@@ -18,11 +43,11 @@ RSpec.describe DocPattern do
     end
   end
 
-  describe '#invalid?' do 
-    it 'should be valid if not had broken rules' do
+  describe "#invalid?" do
+    it "should be valid if not had broken rules" do
       [
-        'lib/json/invalids/ambigous_rule.json',
-        'lib/json/invalids/empty.json'
+        "lib/json/invalids/ambigous_rule.json",
+        "lib/json/invalids/empty.json"
       ].each do |directory|
         subject.read_json(directory)
         expect(subject).to be_invalid
